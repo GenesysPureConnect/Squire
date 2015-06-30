e08ce4488ac82746b31472b1384478f5f32182c1
Nicholas Kamper <nick@nick125.com>
2015-06-30 13:52:50 -0400

    Revert "Fix for extra zero width space in chrome/safari"

--- source/Editor.js
+++ source/Editor.js
@@ -762,7 +762,7 @@ proto._removeFormat = function ( tag, attributes, range, partial ) {
     var doc = this._doc,
         fixer;
     if ( range.collapsed ) {
-        if ( cantFocusEmptyTextNodes && !this._hasZWS ) {
+        if ( cantFocusEmptyTextNodes ) {
             fixer = doc.createTextNode( ZWS );
             this._didAddZWS();
         } else {
