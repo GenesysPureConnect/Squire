80a6cc4c17750c4b6855d4ed722983cfc40003bb
ruiliuinin <Rui.Liu@inin.com>
2016-08-01 15:13:04 -0400

    Add invalid url as a text node instead of a link

--- build/squire-raw.js
+++ build/squire-raw.js
@@ -4005,7 +4005,7 @@ proto.insertImage = function ( src, attributes ) {
     return img;
 };
 
-var linkRegExp = /\b((?:(?:ht|f)tps?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,}\/)(?:[^\s()<>]+|\([^\s()<>]+\))+(?:\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))|([\w\-.%+]+@(?:[\w\-]+\.)+[A-Z]{2,}\b)|(\B\\{2}.+|\bfile:(?:(?:\/\/)|(?:\\{2}))\S+)/i;
+var linkRegExp = /\b((?:(?:ht|f)tps?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,}\/)(?:[^\s()<>]+|\([^\s()<>]+\))+(?:\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))|([\w\-.%+]+@(?:[\w\-]+\.)+[A-Z]{2,}\b)|(\B\\{2}\S+|\bfile:(?:(?:\/\/)|(?:\\{2}))\S+)/i;
 
 var getHref = function( match, self ) {
     var href;
@@ -4068,13 +4068,13 @@ var addLinks = function ( frag, root, self ) {
 
                 child.textContent = data.slice( index, endIndex );
                 parent.insertBefore( child, node );
-                // add link for the valid url and move to the rest data
-                node.data = data = data.slice( endIndex );
             }
             else {
-                // keep the text without adding link for it an move to the rest data
-                data = data = data.slice( endIndex );
-            }            
+                // keep the text without adding link
+                child = doc.createTextNode( data.slice( index, endIndex ) );
+                parent.insertBefore( child, node );
+            }
+            node.data = data = data.slice( endIndex );          
         }
     }
 };
